ce181bbc048db58490eec68bd00d590c42bc85a5
Michael Pack <packmichael@hotmail.com>
2023-04-05 19:56:23 -0230

    handicap function update

--- script.js
+++ script.js
@@ -1,88 +1,48 @@
-function reset() {
-  document.getElementById("txtNum1").value = "0";
-  document.getElementById("txtNum2").value = "0";
-  document.getElementById("txtNum3").value = "0";
-  document.getElementById("txtNum4").value = "0";
-  document.getElementById("txtNum5").value = "0";
-  document.getElementById("txtNum6").value = "0";
-  document.getElementById("txtNum7").value = "0";
-  document.getElementById("txtNum8").value = "0";
-  document.getElementById("txtNum9").value = "0";
-  document.getElementById("txtNum10").value = "0";
-  document.getElementById("txtNum11").value = "0";
-  document.getElementById("txtNum12").value = "0";
-  document.getElementById("txtSum").value = "0";
+
+
+function addInput(event) {
+  event.preventDefault();
+  var form = document.getElementById("form");
+  var newInput = document.createElement("input");
+  newInput.type = "text";
+  form.appendChild(newInput);
 }
 
 function calculate(event) {
-  // event tells it to wait for something to happen before running
-
   var form = document.getElementById("form");
-  // form.addEventListener('submit',function(event)
 
-  event.preventDefault(); // must add when using an event so it doesnt auto run
-  var num1 = document.getElementById("num1").value;
-  var num2 = document.getElementById("num2").value;
-  var num3 = document.getElementById("num3").value;
-  var num4 = document.getElementById("num4").value;
-  var num5 = document.getElementById("num5").value;
-  var num6 = document.getElementById("num6").value;
-  var num7 = document.getElementById("num7").value;
-  var num8 = document.getElementById("num8").value;
-  var num9 = document.getElementById("num9").value;
-  var num10 = document.getElementById("num10").value;
-  var num11 = document.getElementById("num11").value;
-  var num12 = document.getElementById("num12").value;
+  const scorearray = [];
+  Array.from(form.elements).forEach((input) => {
+    if (
+      input.type === "text" &&
+      input.disabled === false &&
+      input.value !== ""
+    ) {
+      scorearray.push(input.value);
+    }
+  });
 
-  console.log(
-    num1,
-    num2,
-    num3,
-    num4,
-    num5,
-    num6,
-    num7,
-    num8,
-    num9,
-    num10,
-    num11,
-    num12
-  );
+  console.log(scorearray);
+  event.preventDefault();
+  
 
-  var valuearray = [
-    num1,
-    num2,
-    num3,
-    num4,
-    num5,
-    num6,
-    num7,
-    num8,
-    num9,
-    num10,
-    num11,
-    num12,
-  ];
-
-  var sorted = valuearray.sort((a, b) => {
+  var sorted = scorearray.sort((a, b) => {
     return a - b;
   });
 
+  console.log(sorted)
   var middle = Math.floor(sorted.length / 2);
 
+  console.log(middle)
   var low = sorted.slice(0, middle);
 
-  console.log(middle);
-
-  console.log(low);
-
+  console.log(low)
   var handicap = low.reduce((acc, curVal) => {
     return Number(acc) + Number(curVal);
   });
 
-  console.log(handicap);
-
-  var sum = handicap / 6;
+  console.log(handicap)
+  var sum = handicap / low.length;
 
   document.getElementById("txtSum").value = sum;
 }
