1078b61c8f641738f234606b2ee70dcaafbb5316
Michael Pack <packmichael@hotmail.com>
2023-05-22 21:43:45 -0230

    editing multple choice quiz

--- script.js
+++ script.js
@@ -224,3 +224,210 @@ function show_hide(
     return;
   }
 }
+
+
+
+(function(){
+
+  function buildQuiz(){
+    
+    const output = [];
+
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+
+        const answers = [];
+
+        for(letter in currentQuestion.answers){
+
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+
+        output.push(
+          `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+          </div>`
+        );
+      }
+    );
+
+    quizContainer.innerHTML = output.join('');
+  }
+
+  buildQuiz();
+  function showResults(){
+
+
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+
+    let numCorrect = 0;
+
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+    
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      if(userAnswer === currentQuestion.correctAnswer){
+      
+        numCorrect++;
+
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+    
+      else{
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+      previousButton.style.display = 'none';
+    }
+    else{
+      previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    }
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
+
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
+  const myQuestions = [
+    {
+      question: "Back in 2014, which golfer became the third player after Jack Nicklaus and Tiger Woods to win three majors by the age of 25 and the first European to win three different major titles.",
+      answers: {
+        a: "Sergio Garcia",
+        b: "John Rahm",
+        c: "Rory McIlroy",
+        d: "Ernie Els"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "How many major championships have Tiger Woods won (to January 2022)?",
+      answers: {
+        a: "15",
+        b: "18",
+        c: "20",
+        d: "12"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "Who is the only tour rookie to win a PGA major?",
+      answers: {
+        a: "Tiger Woods",
+        b: "Rory McIlroy",
+        c: "Jordan Spieth",
+        d: "John Daly"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "What is the appropriate loft range for a sand wedge?",
+      answers: {
+        a: "50-54 Degrees",
+        b: "54-58 Degrees",
+        c: "58-62 Degrees",
+        d: "62-66 Degrees"
+      },
+      correctAnswer: "b"
+    },
+    
+    {
+      question: "An avid golfer, which popular actor has a contract clause to play golf twice a week whenever he films movies?",
+      answers: {
+        a: "Samuel L. Jackson",
+        b: "Bill Murray",
+        c: "Mark Wahlberg",
+        d: "Adam Sandler"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "How many players make the cut at The British Open?",
+      answers: {
+        a: "60",
+        b: "70",
+        c: "80",
+        d: "90"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "How many clubs can a player carry in their bag?",
+      answers: {
+        a: "12",
+        b: "10",
+        c: "15",
+        d: "14"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "How many point are needed to win The Ryder Cup?",
+      answers: {
+        a: "15",
+        b: "10.5",
+        c: "12",
+        d: "14.5"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "What event was Rickie Fowler's first career win?",
+      answers: {
+        a: "Wells Fargo Championship",
+        b: "The Honda Classic",
+        c: "WM Phoenix Open",
+        d: "Valero Texas Open"
+      },
+      correctAnswer: "a"
+    
+  
+    }
+  ];
+
+
+
+  const previousButton = document.getElementById("previous");
+  const nextButton = document.getElementById("next");
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  showSlide(currentSlide)
+
+  submitButton.addEventListener('click', showResults);
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
+});
